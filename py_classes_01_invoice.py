10f5a96944cf16ea0c69388bf321d0888b416f57
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-01-16 18:50:35 +0100

    Summary

---
+++ py_classes_01_invoice.py
@@ -0,0 +1,11 @@
+
+
+racun_broj = 'R-1-2024-50'
+racun_datum_izdavanja = '16.01.2024.'
+racun_stavke = {
+    'Laptop': 499.99,
+    'Torba za laptop': 49.99,
+    'Monitor': 899.99,
+    'Python za pocetnike': 29.99
+}
+racun_ukupna_iznos = 0.00
