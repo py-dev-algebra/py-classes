5db06ed265f12e13bdcb30a10494c52ce9e0dbc3
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-01-16 18:55:57 +0100

    Update py_classes_01_invoice.py

--- py_classes_01_invoice.py
+++ py_classes_01_invoice.py
@@ -8,4 +8,19 @@ racun_stavke = {
     'Monitor': 899.99,
     'Python za pocetnike': 29.99
 }
-racun_ukupna_iznos = 0.00
+racun_ukupan_iznos = 0.00
+for cijena in racun_stavke.values():
+    racun_ukupan_iznos += cijena
+racun_pdv = racun_ukupan_iznos * 0.25
+racun_ukupan_iznos_pdv = racun_ukupan_iznos + racun_pdv
+
+print()
+print(f'RACUN: {racun_broj}')
+print(f'DATUM: {racun_datum_izdavanja}')
+print(f'STAVKE:')
+for proizvod, cijena in racun_stavke.items():
+    print(f'{proizvod}\t{cijena} EUR')
+print(f'DATUM: {racun_ukupan_iznos}')
+print(f'DATUM: {racun_pdv}')
+print(f'DATUM: {racun_ukupan_iznos_pdv}')
+
