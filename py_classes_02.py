ff97989ad959015900eb8cfff4254913193dc0ad
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-01-16 19:27:42 +0100

    Update py_classes_02.py

---
+++ py_classes_02.py
@@ -0,0 +1,23 @@
+televizijski_aparat = {
+    'dijagonala': 55,
+    'sirina': 124,
+    'visina': 79,
+    'proizvodac': 'Grundig',
+    'model': 'Turbo Mega Ultra Extra XYZ 55'
+}
+
+class TelevizijskiAparat:
+    dijagonala = 55
+    sirina = 124
+    visina = 79
+    proizvodac = 'Grundig'
+    model = 'Turbo Mega Ultra Extra XYZ 55'
+
+
+tv_dnevni_boravak = TelevizijskiAparat()
+
+
+print(televizijski_aparat['proizvodac'])
+print(tv_dnevni_boravak.proizvodac)
+print()
+
